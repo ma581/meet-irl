a287acd68e75de43637b0a5392ba69d42d106800
tp50808 <Manoj.Kurien@lv.com>
2017-07-27 14:39:25 +0100

    users find by id

--- app/services/users/users.js
+++ app/services/users/users.js
@@ -6,39 +6,40 @@
         .factory('Users', () => {
             let Users = {};
 
+            const usersList = [
+                {
+                    id: '1',
+                    name: 'Jane',
+                    role: 'Designer',
+                    location: 'New York',
+                    twitter: 'gijane'
+                },
+                {
+                    id: '2',
+                    name: 'Bob',
+                    role: 'Developer',
+                    location: 'New York',
+                    twitter: 'billybob'
+                },
+                {
+                    id: '3',
+                    name: 'Jim',
+                    role: 'Developer',
+                    location: 'Chicago',
+                    twitter: 'jimbo'
+                },
+                {
+                    id: '4',
+                    name: 'Bill',
+                    role: 'Designer',
+                    location: 'LA',
+                    twitter: 'dabill'
+                }
+            ];
 
-            Users.all = function () {
-                return [
-                    {
-                        id: '1',
-                        name: 'Jane',
-                        role: 'Designer',
-                        location: 'New York',
-                        twitter: 'gijane'
-                    },
-                    {
-                        id: '2',
-                        name: 'Bob',
-                        role: 'Developer',
-                        location: 'New York',
-                        twitter: 'billybob'
-                    },
-                    {
-                        id: '3',
-                        name: 'Jim',
-                        role: 'Developer',
-                        location: 'Chicago',
-                        twitter: 'jimbo'
-                    },
-                    {
-                        id: '4',
-                        name: 'Bill',
-                        role: 'Designer',
-                        location: 'LA',
-                        twitter: 'dabill'
-                    }
-                ];
-            };
+            Users.all = ()=>{return usersList};
+
+            Users.findById = id => {};
 
             return Users;
         });
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -4,48 +4,57 @@ describe('Users factory', () => {
 
     beforeEach(angular.mock.module('api.users'));
 
-    beforeEach(inject(_Users_ => {
-        Users = _Users_;
+    beforeEach(inject(($injector) => {
+        Users = $injector.get('Users');
     }));
+
     it('should exist', () => {
         expect(Users).toBeDefined();
     });
 
-    it('should have .#all()', () => {
-        expect(Users.all).toBeDefined();
+    describe('.all()', () => {
+        it('should have .#all()', () => {
+            expect(Users.all).toBeDefined();
+        });
+
+        it('should return list of users', () => {
+            const usersList = [
+                {
+                    id: '1',
+                    name: 'Jane',
+                    role: 'Designer',
+                    location: 'New York',
+                    twitter: 'gijane'
+                },
+                {
+                    id: '2',
+                    name: 'Bob',
+                    role: 'Developer',
+                    location: 'New York',
+                    twitter: 'billybob'
+                },
+                {
+                    id: '3',
+                    name: 'Jim',
+                    role: 'Developer',
+                    location: 'Chicago',
+                    twitter: 'jimbo'
+                },
+                {
+                    id: '4',
+                    name: 'Bill',
+                    role: 'Designer',
+                    location: 'LA',
+                    twitter: 'dabill'
+                }
+            ];
+            expect(Users.all()).toEqual(usersList);
+        });
     });
 
-    it('should return list of users', () => {
-        const usersList = [
-            {
-                id: '1',
-                name: 'Jane',
-                role: 'Designer',
-                location: 'New York',
-                twitter: 'gijane'
-            },
-            {
-                id: '2',
-                name: 'Bob',
-                role: 'Developer',
-                location: 'New York',
-                twitter: 'billybob'
-            },
-            {
-                id: '3',
-                name: 'Jim',
-                role: 'Developer',
-                location: 'Chicago',
-                twitter: 'jimbo'
-            },
-            {
-                id: '4',
-                name: 'Bill',
-                role: 'Designer',
-                location: 'LA',
-                twitter: 'dabill'
-            }
-        ];
-        expect(Users.all()).toEqual(usersList);
-    })
+    describe('.findById()', ()=> {
+        it('should exist', ()=>{
+            expect(Users.findById).toBeDefined();
+        });
+    });
 });
